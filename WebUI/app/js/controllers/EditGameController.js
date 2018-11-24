bb1112957dfb0165226c0f4d687bdf286499e3d4
Szabo Robert <robert.szabo@arnia.ro>
2018-11-24 15:27:28 +0200

    save & cancel draft

--- WebUI/app/js/controllers/EditGameController.js
+++ WebUI/app/js/controllers/EditGameController.js
@@ -5,14 +5,30 @@ chessApp.controller('EditGameController',
         
         $scope.game = {};
         $scope.game.result = "1";
+        $scope.canCreateGame = false
 
         playersService.getPlayers(function OnPlayersLoaded(players) {
             $scope.players = players;
             $scope.game.whitePlayer = players[0];
             $scope.game.blackPlayer = players[1];
+            $scope.canCreateGame = areAtLeastTwoPlayers(players);
         }) ;
 
+        function areAtLeastTwoPlayers(players) {
+            if(players === undefined || players === null) {
+                return false;
+            }
+            return players.length > 2;
+        }
         
-        
+        $scope.saveGame = function(game, gameForm) {
+            if(gameForm.$valid) {
+                alert('ok');
+            }
+        }
+
+        $scope.cancel = function() {
+            $location.url("/players");
+        }
     }
 );
