55d31791c32a654e1c2195f9c5eaf99d85e79f3e
Szabo Robert <robert.szabo@arnia.ro>
2018-11-24 20:28:52 +0200

    save game ddraft

--- WebUI/app/js/services/GamesService.js
+++ WebUI/app/js/services/GamesService.js
@@ -1,7 +1,26 @@
 chessApp.factory('gamesService', function($http, $log) {
     return {
-        saveGame: function(game) {
+        saveGame: function(game, successCallback) {
             $log.info(game);
+            var gameDto = createDtoFrom(game);
+            $http.post('/api/games', gameDto)
+            .success(function(data, status, headers, config) {
+                successCallback(data);          
+            })
+            .error(function(data, status, headers, config) {
+                $log.warn(data, status, headers, config);          
+            });
         }
+        
     };
+
+    function createDtoFrom(game) {
+        return {
+            whitePlayer: game.whitePlayer.playerId,
+            blackPlayer: game.blackPlayer.playerId,
+            gameDate: game.gameDate,
+            openingClassification: game.openingClassification,
+            result: game.result
+        };
+    }
 })
