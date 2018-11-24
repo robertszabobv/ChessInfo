f6c37e03fbc0c8b1e78524a849dfd8b8f0717062
Szabo Robert <robert.szabo@arnia.ro>
2018-11-24 15:14:47 +0200

    default selections

--- WebUI/app/js/controllers/EditGameController.js
+++ WebUI/app/js/controllers/EditGameController.js
@@ -4,11 +4,12 @@ chessApp.controller('EditGameController',
     function EditGameController($scope, playersService, $routeParams, $location, $log) {
         
         $scope.game = {};
+        $scope.game.result = "1";
 
         playersService.getPlayers(function OnPlayersLoaded(players) {
             $scope.players = players;
-            $scope.selectedWhitePlayer = players[0];
-            $scope.selectedBlackPlayer = players[1];
+            $scope.game.whitePlayer = players[0];
+            $scope.game.blackPlayer = players[1];
         }) ;
 
         
