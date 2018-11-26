70c1d5a05a71ea21ce2399518c3d4f1162936e92
Szabo Robert <robert.szabo@arnia.ro>
2018-11-26 21:51:50 +0200

    adding GamesTests - integration

---
+++ ChessIno.Api/ChessInfo.Api.IntegrationTests/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessInfo.Api.IntegrationTests
+{
+    internal class Game
+    {
+        public int GameId { get; set; }
+        public int WhitePlayerId { get; set; }
+        public int BlackPlayerId { get; set; }
+        public DateTime GameDate { get; set; }
+        public string OpeningClassification { get; set; }
+        public byte GameResult { get; set; }
+    }
+}
---
+++ ChessIno.Api/ChessInfo.Api.IntegrationTests/GamesTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FizzWare.NBuilder;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace ChessInfo.Api.IntegrationTests
+{
+    [TestFixture]
+    public class GamesTests
+    {
+
+        private const string PlayersRelativeUrl = "games";
+        const string UpdatedLastName = "updated by http put";
+
+        [Test]
+        public void CRUD_Succeeds()
+        {
+
+        }
+
+        private Game CreateDummyGame()
+        {            
+            return Builder<Game>.CreateNew()
+                .With(g => g.WhitePlayerId = 1)
+                .With(g => g.BlackPlayerId = 2)
+                .With(g => g.GameDate == DateTime.Now)
+                .With(g => g.OpeningClassification = "A12")
+                .With(g => g.GameResult = 1)
+                .Build();
+        }
+    }
+}
