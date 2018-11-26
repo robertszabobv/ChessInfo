6753a09e295631d45b16773e2f0a35f6267885f8
Szabo Robert <robert.szabo@arnia.ro>
2018-11-26 23:06:05 +0200

    integration tests category

--- ChessIno.Api/ChessInfo.Api.IntegrationTests/GamesTests.cs
+++ ChessIno.Api/ChessInfo.Api.IntegrationTests/GamesTests.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 namespace ChessInfo.Api.IntegrationTests
 {
     [TestFixture]
+    [Category("Integration tests")]
     public class GamesTests
     {
         private const string OpeningClassificationInitial = "A12";
